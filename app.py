17c48b360d26a906ca7f1ac4dde70ec3b4cffa30
asdy10 <asdyasdfghjkl@yandex.ru>
2025-03-03 22:27:26 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+import pika
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS")
+QUEUE_NAME = 'errors'
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+USER_ID = os.getenv("USER_ID")
+
+
+def receive_messages():
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+    def callback(ch, method, properties, body):
+        try:
+            data = json.loads(body.decode('utf-8'))
+            text = data_to_text(data)
+            send_message(text)
+        except Exception as e:
+            print(e)
+
+    channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback)
+    channel.start_consuming()
+
+
+def data_to_text(data):
+    s = f'''{data['source']}
+{data['text']}'''
+    return s
+
+
+def send_message(message):
+    api_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": USER_ID, "text": message, 'parse_mode': 'HTML'}
+    requests.post(api_url, json=payload)
+
+
+if __name__ == '__main__':
+    receive_messages()
